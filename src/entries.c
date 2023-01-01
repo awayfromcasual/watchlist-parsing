efcd4065e952dbfb70f9159fa51cfc8dfe22ef6a
Fen <awayfromcasual@gmail.com>
2023-01-01 02:54:46 -0500

    Implemented entry functionality

--- src/entries.c
+++ src/entries.c
@@ -1,3 +1,48 @@
 
 #include "entries.h"
 #include "dates.h"
+
+#include <stdio.h>
+#include <string.h>
+
+void entry_to_string(entry_t e, char* dest)
+{
+    char start_str[20], end_str[20];
+    date_to_string(e.start_date, start_str);
+    date_to_string(e.end_date, end_str);
+
+    // Store formatted string
+    sprintf(dest, "%s\t%s\t%s\t%d", start_str, end_str, e.title,
+            e.score);
+}
+
+entry_t string_to_entry(char* src)
+{
+    // Parse input
+    char start_str[20], end_str[20], title_str[100];
+    int score;
+    sscanf(src, "%s %s %[^\t] %d", start_str, end_str, title_str,
+           &score);
+
+    // Create dates from strings
+    date_t start_date = string_to_date(start_str);
+    date_t end_date = string_to_date(end_str);
+
+    // Generate result
+    entry_t res = (entry_t){start_date, end_date, "", score};
+    strcpy(res.title, title_str);
+
+    return res;
+}
+
+void print_entry(entry_t e, char c)
+{
+    char text[150];
+    entry_to_string(e, text);
+    printf("%s%c", text, c);
+}
+
+void sort_entries(entry_t* entries, int num_entries, int asc)
+{
+    ;
+}
--- src/entries.h
+++ src/entries.h
@@ -3,20 +3,20 @@
 
 #include "dates.h"
 
-#define MAX_TITLE_LENGTH 100
+#define TITLE_SIZE 100
 
 typedef struct entry
 {
     date_t start_date;
     date_t end_date;
-    char title[MAX_TITLE_LENGTH];
+    char title[TITLE_SIZE];
     int score;
 } entry_t;
 
 void entry_to_string(entry_t e, char* dest);
 
-void string_to_entry(char* src);
+entry_t string_to_entry(char* src);
 
-void print_entry(entry_t e);
+void print_entry(entry_t e, char c);
 
 void sort_entries(entry_t* entries, int num_entries, int asc);
