086d4ddc6f6530d044c8e381edc4c8ac499f58f5
Fen <awayfromcasual@gmail.com>
2023-01-01 00:17:39 -0500

    Bug fixes

--- src/main.c
+++ src/main.c
@@ -1,26 +1,25 @@
 
 #include <stdio.h>
 
-#define BUF_SIZE 150
+#define BUF_SIZE 200
 
 int main()
 {
     printf("Hello, Fen!\n");
 
-    char input[200];
+    char input[BUF_SIZE];
 
     fgets(input, BUF_SIZE, stdin);
 
-    char start_date[20], end_date[20], title[100], score[10];
-    int score;
+    char start_date[13], end_date[13], title[100], score[4];
 
-    sscanf(input, "%s %s %[^\t] %d", start_date, end_date, title,
+    sscanf(input, "%s %s %[^\t] %s", start_date, end_date, title,
            score);
 
     printf("%s\n", start_date);
     printf("%s\n", end_date);
     printf("%s\n", title);
-    printf("%d\n", score);
+    printf("%s\n", score);
 
     return 0;
 }
