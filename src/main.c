b6c7f86540b6bfee872264267228d01fbf82fd57
Fen <awayfromcasual@gmail.com>
2023-01-01 00:11:24 -0500

    Added score

--- src/main.c
+++ src/main.c
@@ -12,12 +12,15 @@ int main()
     fgets(input, BUF_SIZE, stdin);
 
     char start_date[13], end_date[13], title[90];
+    int score;
 
-    sscanf(input, "%s %s %[^\n]", start_date, end_date, title);
+    sscanf(input, "%s %s %[^\t] %d", start_date, end_date, title,
+           &score);
 
     printf("%s\n", start_date);
     printf("%s\n", end_date);
     printf("%s\n", title);
+    printf("%d\n", score);
 
     return 0;
 }
