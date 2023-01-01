6e34db9b0391d7d94135b7fa7925f55e7e55660b
Fen <awayfromcasual@gmail.com>
2023-01-01 00:08:49 -0500

    Implemented line parsing

--- src/main.c
+++ src/main.c
@@ -1,8 +1,23 @@
 
 #include <stdio.h>
 
+#define BUF_SIZE 120
+
 int main()
 {
-    printf("Hello, Fen!");
+    printf("Hello, Fen!\n");
+
+    char input[BUF_SIZE];
+
+    fgets(input, BUF_SIZE, stdin);
+
+    char start_date[13], end_date[13], title[90];
+
+    sscanf(input, "%s %s %[^\n]", start_date, end_date, title);
+
+    printf("%s\n", start_date);
+    printf("%s\n", end_date);
+    printf("%s\n", title);
+
     return 0;
 }
