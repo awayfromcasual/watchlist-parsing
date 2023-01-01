6df568653329b377f7254648ca18228a4ed18d62
Fen <awayfromcasual@gmail.com>
2023-01-01 01:25:09 -0500

    Improved how entries are read. No longer counting lines before reading.

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 
+#include "dates.h"
+
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUF_SIZE 200
+#define BUFFER_SIZE 200
 
 int main(int argc, char** argv)
 {
@@ -12,24 +14,12 @@ int main(int argc, char** argv)
     FILE* file_ptr;
     file_ptr = fopen(argv[1], "r");
 
-    // Count lines in file
-    int entries = 0;
-    for (char c = getc(file_ptr); c != EOF; c = getc(file_ptr))
+    // Input buffer
+    char input[BUFFER_SIZE];
+
+    while (fgets(input, BUFFER_SIZE, file_ptr) != NULL
+           && *input != '\n')
     {
-        if (c == '\n')
-            entries = entries + 1;
-    }
-
-    file_ptr = fopen(argv[1], "r");
-
-    printf("Read %d entries\n", entries);
-
-    for (int i = 0; i < entries; i++)
-    {
-        // Get input from command line
-        char input[BUF_SIZE];
-        fgets(input, BUF_SIZE, file_ptr);
-
         // Parse input
         char start_date[13], end_date[13], title[100], score[4];
         sscanf(input, "%s %s %[^\t] %s", start_date, end_date,
